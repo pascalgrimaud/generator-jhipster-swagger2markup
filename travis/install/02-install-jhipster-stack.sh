1bab689e344babd918f6a0ed498f4f32071e69a3
Pascal Grimaud <pascalgrimaud@gmail.com>
2015-12-25 19:00:13 +0100

    Clean up and Wording

--- travis/install/02-install-jhipster-stack.sh
+++ travis/install/02-install-jhipster-stack.sh
@@ -9,7 +9,7 @@ npm install -g grunt-cli
 npm install -g gulp
 npm install -g generator-jhipster
 #-------------------------------------------------------------------------------
-# Install the latest version of generator-jhipster-docker
+# Install the latest version of generator-jhipster-swagger2markup
 #-------------------------------------------------------------------------------
 cd $TRAVIS_BUILD_DIR/
 npm install
